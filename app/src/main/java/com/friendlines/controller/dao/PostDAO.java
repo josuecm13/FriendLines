a5bcb6c34bdf833e5c94ec1b8d92edab761991b9
Eduardo <emjironal@gmail.com>
2019-06-10 11:22:23 -0600

    Avance en post

--- app/src/main/java/com/friendlines/controller/dao/PostDAO.java
+++ app/src/main/java/com/friendlines/controller/dao/PostDAO.java
@@ -1,3 +1,16 @@
 package com.friendlines.controller.dao;
 
-public class PostDAO { }
+import com.friendlines.model.post.Post;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class PostDAO
+{
+    private static final String COLLECTION_NAME = "posts";
+
+    public PostDAO(){}
+
+    public void addPost(Post post)
+    {
+        FirebaseFirestore.getInstance().collection(COLLECTION_NAME).add(post);
+    }
+}
--- app/src/main/java/com/friendlines/view/CreatePostActivity.java
+++ app/src/main/java/com/friendlines/view/CreatePostActivity.java
@@ -1,15 +1,23 @@
 package com.friendlines.view;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.friendlines.R;
+import com.friendlines.controller.Controller;
 
 public class CreatePostActivity extends AppCompatActivity {
 
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
     ActionBar actionBar;
+    Controller controller;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,9 +25,13 @@ public class CreatePostActivity extends AppCompatActivity {
         actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setTitle("Create Post");
+
+        controller = Controller.getInstance();
     }
 
-    public void addImage(View view) {
+    public void addImage(View view)
+    {
+        openGallery();
     }
 
     public void addVideo(View view) {
@@ -29,6 +41,23 @@ public class CreatePostActivity extends AppCompatActivity {
         //Maybe optional
     }
 
-    public void submit(View view) {
+    public void submit(View view)
+    {
+
+    }
+
+    public void openGallery()
+    {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(resultCode == RESULT_OK && requestCode == PICK_IMAGE)
+        {
+            imageUri = data.getData();
+        }
     }
 }
