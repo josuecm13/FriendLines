27b5d6790755dc7ffdb88b86812021a7fb87be24
Eduardo <emjironal@gmail.com>
2019-06-11 15:34:57 -0600

    Load frienships

--- app/src/main/java/com/friendlines/view/MainActivity.java
+++ app/src/main/java/com/friendlines/view/MainActivity.java
@@ -5,12 +5,15 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.friendlines.R;
 import com.friendlines.controller.ControlException;
 import com.friendlines.controller.Controller;
+import com.friendlines.controller.listeners.FriendshipEventListener;
 import com.friendlines.controller.listeners.UserEventListener;
+import com.friendlines.model.Friendship;
 import com.friendlines.model.User;
 import com.friendlines.view.homefragments.FriendsFragment;
 import com.friendlines.view.homefragments.NotificationsFragment;
@@ -29,6 +32,7 @@ public class MainActivity extends AppCompatActivity
     private NotificationsFragment notificationsFragment;
     private SearchFragment searchFragment;
     private boolean firstTime;
+    private Controller controller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -78,9 +82,38 @@ public class MainActivity extends AppCompatActivity
         }
         //ejecutar todo lo necesario cuando llegue cualquier modificación al objeto de User
         profileFragment.onUserChanged(user);
+        controller = Controller.getInstance();
+        loadFriendships();
     }
 
     private void userWasDeleted(){
 
+    }
+
+    private void loadFriendships()
+    {
+        try
+        {
+            controller.listenFriendship(this, controller.getDto().getUser().getId(), new FriendshipEventListener() {
+                @Override
+                public void onFriendshipAdded(Friendship friendship) {
+                    controller.getDto().getFriendships().add(friendship);
+                }
+
+                @Override
+                public void onFriendshipAccepted(Friendship friendship) {
+                    controller.getDto().getFriendships().set(controller.getDto().getFriendships().indexOf(friendship), friendship);
+                }
+
+                @Override
+                public void onFriendshipRejected(Friendship friendship) {
+                    controller.getDto().getFriendships().remove(friendship);
+                }
+            });
+        }
+        catch (ControlException e)
+        {
+            Log.e("Error", e.getMessage());
+        }
     }
 }
