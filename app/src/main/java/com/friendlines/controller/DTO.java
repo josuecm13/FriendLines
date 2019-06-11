7008326b64ed7ec0c71b2bacb61bdbf64352729e
Eduardo <emjironal@gmail.com>
2019-06-11 11:39:30 -0600

    Avance getposts

--- app/src/main/java/com/friendlines/controller/DTO.java
+++ app/src/main/java/com/friendlines/controller/DTO.java
@@ -5,6 +5,9 @@ import com.friendlines.model.Friendship;
 import com.friendlines.model.post.Post;
 import com.friendlines.model.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DTO
 {
     private User user;
@@ -12,8 +15,16 @@ public class DTO
     private Friendship friendship;
     private Post post;
     private Post comment;
+    private List<Post> posts;
+    private List<Post> comments;
+    private List<Friendship> friendships;
 
-    public DTO(){ }
+    public DTO()
+    {
+        posts = new ArrayList<>();
+        comments = new ArrayList<>();
+        friendships = new ArrayList<>();
+    }
 
     public User getUser()
     {
@@ -21,6 +32,12 @@ public class DTO
             user = new User();
         return user;
     }
+
+    public List<Friendship> getFriendships(){return friendships;}
+
+    public List<Post> getComments(){return comments;}
+
+    public List<Post> getPosts(){return posts;}
 
     public void setUser(User user) {
         this.user = user;
--- app/src/main/java/com/friendlines/view/homefragments/UserFeedFragment.java
+++ app/src/main/java/com/friendlines/view/homefragments/UserFeedFragment.java
@@ -5,12 +5,17 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.friendlines.R;
+import com.friendlines.controller.ControlException;
+import com.friendlines.controller.Controller;
 import com.friendlines.controller.adapters.PostsAdapter;
+import com.friendlines.controller.listeners.PostEventListener;
+import com.friendlines.model.Friendship;
 import com.friendlines.model.post.Post;
 import com.google.firebase.Timestamp;
 
@@ -25,6 +30,7 @@ public class UserFeedFragment extends Fragment {
 
     PostsAdapter adapter;
     RecyclerView recyclerView;
+    Controller controller;
 
     public UserFeedFragment() {
         // Required empty public constructor
@@ -42,11 +48,55 @@ public class UserFeedFragment extends Fragment {
         for (int i = 0; i < 19; i++) {
             postList.add(post);
         }
-        adapter = new PostsAdapter(postList, getContext());
+        adapter = new PostsAdapter(controller.getDto().getPosts(), getContext());
         recyclerView = view.findViewById(R.id.recyclerview);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(adapter);
+        controller = Controller.getInstance();
+        loadPosts();
         return view;
     }
 
+    private void loadPosts()
+    {
+        try
+        {
+            for(Friendship friendship : controller.getDto().getFriendships())
+            {
+                if(friendship.getStatus().equals(Friendship.ACCEPTED_STATUS))
+                {
+                    controller.listenPost(getActivity(), friendship.getId(), new PostEventListener() {
+                        @Override
+                        public void onPostAdded(Post post)
+                        {
+                            controller.getDto().getPosts().add(post);
+                            adapter.notifyDataSetChanged();
+                        }
+
+                        @Override
+                        public void onPostChanged(Post post)
+                        {
+                            controller.getDto().getPosts().set(controller.getDto().getPosts().indexOf(post), post);
+                            adapter.notifyDataSetChanged();
+                        }
+
+                        @Override
+                        public void onPostDeleted(Post post)
+                        {
+                            controller.getDto().getPosts().remove(post);
+                            adapter.notifyDataSetChanged();
+                        }
+                    });
+                }
+            }
+        }
+        catch (ControlException e)
+        {
+            Log.e("Error", e.getMessage());
+        }
+    }
+
+    private void deletePost(int i)
+    {
+    }
 }
