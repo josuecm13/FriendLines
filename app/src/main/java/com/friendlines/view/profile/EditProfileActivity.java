3f652db0a95039b7e4f5d29da3f462561c3b31da
Josue Canales <josuecanales0@gmail.com>
2019-06-12 09:27:30 -0600

    cambios

--- app/src/main/java/com/friendlines/view/profile/EditProfileActivity.java
+++ app/src/main/java/com/friendlines/view/profile/EditProfileActivity.java
@@ -1,23 +1,29 @@
 package com.friendlines.view.profile;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import com.friendlines.R;
 import com.friendlines.controller.ControlException;
 import com.friendlines.controller.Controller;
 import com.friendlines.model.User;
+import com.friendlines.view.auth.register.RegBirthGenderActivity;
 import com.google.firebase.Timestamp;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class EditProfileActivity extends AppCompatActivity
 {
 
     private Controller controller;
+    int year, month, day;
 
     EditText email, number, firstName, lastName, city, gender, birth;
 
@@ -40,6 +46,10 @@ public class EditProfileActivity extends AppCompatActivity
         gender = findViewById(R.id.gender_edit_text);
         birth = findViewById(R.id.bday_edit_text);
         email.setText(user.getEmail());
+        Date date = user.getBirthday().toDate();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        String dateString = sdf.format(date);
+        birth.setText(dateString);
         //number.setText(user.getPhone());
         firstName.setText(user.getFirstname());
         lastName.setText(user.getLastname());
