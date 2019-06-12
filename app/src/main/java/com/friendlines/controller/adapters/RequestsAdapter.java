3ee955bd4e58095c0fc49ca229000a0e6e2913d3
Eduardo Jirón <emjironal@gmail.com>
2019-06-12 01:46:45 -0600

    Aceptar o rechazar solicitud

--- app/src/main/java/com/friendlines/controller/adapters/RequestsAdapter.java
+++ app/src/main/java/com/friendlines/controller/adapters/RequestsAdapter.java
@@ -3,12 +3,15 @@ package com.friendlines.controller.adapters;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.friendlines.R;
+import com.friendlines.controller.ControlException;
+import com.friendlines.controller.Controller;
 import com.friendlines.model.Friendship;
 import com.mikhaellopez.circularimageview.CircularImageView;
 import com.squareup.picasso.Picasso;
@@ -37,7 +40,7 @@ public class RequestsAdapter  extends RecyclerView.Adapter<RequestsAdapter.Holde
 
     @Override
     public void onBindViewHolder(@NonNull Holder holder, int i) {
-        Friendship f = friendshipList.get(i);
+        final Friendship f = friendshipList.get(i);
         holder.name.setText(f.getSender_name());
         Picasso.with(context).load(f.getSender_image()).into(holder.image);
         holder.image.setOnClickListener(new View.OnClickListener() {
@@ -48,14 +51,29 @@ public class RequestsAdapter  extends RecyclerView.Adapter<RequestsAdapter.Holde
         });
         holder.accept.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                //
+            public void onClick(View v)
+            {
+                try
+                {
+                    Controller.getInstance().acceptFriendship(f.getId());
+                }
+                catch (ControlException e)
+                {
+                    Log.e("Error", e.getMessage());
+                }
             }
         });
         holder.decline.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //
+                try
+                {
+                    Controller.getInstance().rejectFriendship(f.getId());
+                }
+                catch (ControlException e)
+                {
+                    Log.e("Error", e.getMessage());
+                }
             }
         });
     }
