bf7cd1eae3bc9d2ab498fa26607ab1ad3b3312a7
Eduardo <emjironal@gmail.com>
2019-06-11 18:04:16 -0600

    Error capa 8

--- app/src/main/java/com/friendlines/view/MainActivity.java
+++ app/src/main/java/com/friendlines/view/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        controller = Controller.getInstance();
         userFeedFragment = new UserFeedFragment();
         friendsFragment = new FriendsFragment();
         profileFragment = new ProfileFragment();
